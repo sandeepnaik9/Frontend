121cb905954d9eca78dbde7a3cbcca66beb5ba99
sandeepnaik9 <sandeepnaik9900@gamil.com>
2023-05-04 00:02:20 +0530

    Landing Page Sample

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import Home from "./pages/homepage";
+import {useState,useEffect} from 'react'
 import { Route,Routes } from 'react-router-dom';
 function App() {
+  const [deviceType, setDeviceType] = useState("")
+
+  useEffect(() => {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone/i.test(
+        navigator.userAgent
+      )
+    ) {
+      setDeviceType("Mobile");
+    } else {
+      setDeviceType("Desktop");
+    }
+  }, []);
   return (<>
-      <Routes>
+      {deviceType==="Mobile"?(<h3>Website is not yet responsive kindly use laptop or pc</h3>):(<Routes>
         <Route path="/" element={<Home />} />
-      </Routes>
+      </Routes>)}
   </>
   )
 }
